7a2c16df0b44cb58f9760dcd368c1be1304cb192
Ayyanar <48320634+ayyanar-codes@users.noreply.github.com>
2023-12-11 12:12:40 +0530

    Add files via upload

---
+++ 5.Pipes/2.Other-Pipe/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '2.Other-Pipe';
+  message='Hello World';
+  currency:number=300;
+  sampleNumber:number=3.6;
+}
