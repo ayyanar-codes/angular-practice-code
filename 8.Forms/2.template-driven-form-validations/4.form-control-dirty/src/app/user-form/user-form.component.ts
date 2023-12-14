f9cd70e12a81a213786d9eddf3c57151a3669a14
Ayyanar <48320634+ayyanar-codes@users.noreply.github.com>
2023-12-14 19:49:55 +0530

    Add files via upload

---
+++ 8.Forms/2.template-driven-form-validations/4.form-control-dirty/src/app/user-form/user-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Form } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+
+  constructor() { }
+
+  user!:User;
+
+  ngOnInit(): void {
+
+    this.user={
+      firstName:'',
+      lastName:'A',
+      email:'ayanarya@outlook.com',
+      gender:'male',
+      isMarried:true
+    }
+
+    // this.user={
+    //   firstName:'Ayyyanar',
+    //   lastName:'A',
+    //   email:'ayanarya@outlook.com',
+    //   gender:'male',
+    //   isMarried:true
+    // }
+
+  }
+
+  fnSubmit(form:Form){
+    console.log(form);
+  }
+
+}
+
+class User{
+  firstName!:string;
+  lastName!:string;
+  email!:string;
+  gender!:string;
+  isMarried!:boolean;
+}
